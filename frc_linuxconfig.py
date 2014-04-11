e04ae5b495c308211e5e1f123315a427a2987416
kira01 <user.name>
2014-04-11 13:46:03 +0100

    add getJitsi()

---
+++ frc_linuxconfig.py
@@ -0,0 +1,51 @@
+#Fly Rod Crosby: Making Cryptoparties Easier since 1897!
+import os
+
+
+
+home=os.path.expanduser("~")
+hkps_cert_link=u'https://sks-keyservers.net/sks-keyservers.netCA.pem'
+gpg_file_name='gpg_to_install.tar.bz2'
+GPGurl=u'http://www.gnupg.org/download/'
+tor_url = u'https://www.torproject.org/projects/torbrowser.html.en#Download-torbrowserbundle'
+jitsi_url = u'https://download.jitsi.org/jitsi/debian/'
+#SOMEONE PLEASE CHECK THAT I GOT ERINN CLARK'S GPG INFO RIGHT
+tor_dev_gpg_fingerprint = '8738A680B84B3031A630F2DB416F061063FEE659'
+tor_dev_gpg_name = u'Erinn Clark (erinn@torproject.org)'
+enigmail_dev_gpg_fingerprint = u'10B2E4A0E718BB1B2791DAC4F040E41B9369CDF3'
+gpg_servers = ['pool.sks-keyservers.net','subkeys.pgp.net','sks.mit.edu','pgp.mit.edu']
+#TO-DO: We need to substitute whatever the language variable needs to be in here
+cryptocat_url = u'https://addons.mozilla.org/en-US/firefox/addon/cryptocat/'
+enigmail_url = u'https://www.enigmail.net/download/download-static.php'
+thunderbird_url = u'https://www.mozilla.org/en-US/thunderbird/all.html'
+bleachbit_url = u'http://bleachbit.sourceforge.net/download/windows'
+bleachbit_base_url = u'http://bleachbit.sourceforge.net/download/'
+tails_url = u'https://tails.boum.org/download/'
+torbirdy_xpi_url = u'https://www.torproject.org/dist/torbirdy/torbirdy-current.xpi'
+torbirdy_xpi_sig_url = u'https://www.torproject.org/dist/torbirdy/torbirdy-current.xpi.asc'
+#You can find this key by searching for "Jacob Appelbaum (offline long term identity key)"
+torbirdy_dev_gpg_fingerprint = u'228FAD203DE9AE7D84E25265CF9A6F914193A197'
+url_trueCrypt=u'http://www.truecrypt.org/dl'
+jitsi_url = u'https://jitsi.org/Main/Download'
+tails_finger_print='0D24B36AA9A2A651787876451202821CBE2CD9C1'
+FakeDomain_url=u'https://addons.mozilla.org/ro/firefox/addon/fake-domain/'
+
+
+#try:
+    #os.environ['PROGRAMFILES(X86)']
+    #arch=64
+#except KeyError:
+    #arch=32
+
+#if arch == 64:
+    #thunderbird_ext_dir = 'C:\\Program Files (x86)\\Mozilla Thunderbird\\extensions\\'
+    #thunderbird_main_dir = r'""C:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe""'
+    #gpg4win_path = 'C:\\Program\ Files\ (x86)\\GNU\\GnuPG\\gpg2.exe'
+    #tbird_reg_str = r'SOFTWARE\Wow6432Node\Mozilla\Mozilla Thunderbird\Extensions'
+#else:
+    #thunderbird_ext_dir = 'C:\\Program Files\\Mozilla Thunderbird\\extensions\\'
+    #thunderbird_main_dir = r'""C:\Program Files\Mozilla Thunderbird\thunderbird.exe""'
+    #gpgrwin_path = 'C:\\Program\ Files\\GNU\\GnuPG\\gpg2.exe'
+    #tbird_reg_str = r'SOFTWARE\Mozilla\Mozilla Thunderbird\Extensions'
+
+
